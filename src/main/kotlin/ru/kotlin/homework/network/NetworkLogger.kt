8c361f35b3479a3ad908d12bb61d596f43ba7327
iamwhoiam0 <gamzatgasanov2337@mail.ru>
2025-03-30 01:30:00 +0300

    done

--- src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
+++ src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
@@ -15,7 +15,7 @@ sealed class ApiException(message: String) : Throwable(message) {
     data object UnknownException: ApiException("Unknown exception")
 }
 
-class ErrorLogger<E : Throwable> {
+class ErrorLogger< E: Throwable> {
 
     val errors = mutableListOf<Pair<LocalDateTime, E>>()
 
@@ -30,9 +30,13 @@ class ErrorLogger<E : Throwable> {
             println("Error at $date: ${error.message}")
         }
     }
+
+    fun dump(): List<Pair<LocalDateTime, E>>{
+        return errors
+    }
 }
 
-fun processThrowables(logger: ErrorLogger<Throwable>) {
+fun <T:ErrorLogger<Throwable>>processThrowables(logger: T) {
     logger.log(Success("Success"))
     Thread.sleep(100)
     logger.log(Success(Circle))
@@ -42,7 +46,7 @@ fun processThrowables(logger: ErrorLogger<Throwable>) {
     logger.dumpLog()
 }
 
-fun processApiErrors(apiExceptionLogger: ErrorLogger<ApiException>) {
+fun <T: ErrorLogger<in ApiException>>processApiErrors(apiExceptionLogger: T) {
     apiExceptionLogger.log(Success("Success"))
     Thread.sleep(100)
     apiExceptionLogger.log(Success(Circle))
@@ -60,5 +64,7 @@ fun main() {
 
     println("Processing Api:")
     processApiErrors(logger)
+
+    println(logger.dump())
 }
 
